b87df8f24ea3897ac94e1780d8f7353ef9aab0a8
juliarefai28-lgtm <julia_refai28@milton.edu>
2026-02-04 12:48:08 -0500

    Revisions for loop homework

--- src/Loops.java
+++ src/Loops.java
@@ -7,8 +7,8 @@ public class Loops {
 //constructor that calls the methods in order
     public Loops(){
         countUp();
-        countDown();
         countByThrees();
+        countDown();
         nestedLoop();
     }
 // Method#1 loop that prints number 1 to 5
@@ -20,23 +20,26 @@ public class Loops {
     //Method#2 loop that prints number 3-15 by 3's
     public void countByThrees(){
         for (int i=3;i<=15;i+=3) {
-            System.out.println(i);
+            System.out.print(i+" ");
         }
     }
     //Method#3 loop that prints numbers from 10-1 by 1's
     public void countDown(){
+        System.out.println( " ");
         for (int i=10; i>=1; i--) {
-            System.out.println(i);
-            System.out.println("Happy Loop Year");
+            System.out.print(i+", ");
         }
+        System.out.println(" ");
+        System.out.println("Happy Loop Year");
     }
     //Method#4 nested loop that prints a triangle where each row shoes the number repeated that many times
     public void nestedLoop() {
-        for (int i = 1; i <= 5; i++) {
-            for (int x = 1; x <= i; x++) {
-                System.out.println(i);
+        for (int i = 1; i < 6; i=i+1) {
+            System.out.println(" ");
+            for (int x = 1; x <= i; x=x+1) {
+                System.out.print( i+ " ");
             }
         }
     }
-
+    
     }
